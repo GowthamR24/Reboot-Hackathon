64df1e9106b8cb8309371ee3343eb59302676660
Gowtham Ravichandran <gowthamravichandran@macusers-MacBook-Pro-3.local>
2020-02-12 10:57:42 +0000

    moneytree

---
+++ src/components/dial.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Dial extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import { Segment } from 'semantic-ui-react'
 export default class Header extends Component {
     render() {
         return (
-            <Segment inverted id="header">I'm a header !</Segment>
+            <Segment inverted id="header">How Green am I ?</Segment>
         )
     }
 }
